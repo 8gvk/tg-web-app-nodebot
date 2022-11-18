d1824b6e7ff2a082a00452bca8b0061fd856235f
vvk89 <klyuchnikvitaly@yandex.ru>
2022-11-18 15:07:49 +0300

    initial

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post('/web-data', async (req, res) => {
     try {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
-            id: queryId,
+            id: 'queryId',
             title: 'Успешная покупка',
             input_message_content: {
                 message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
